fe3ba3d0676ac14c4c01a0974e8bfeac2c79812c
AyoSho <sholarinb@gmail.com>
2022-07-15 22:08:41 +0000

    1. Created for loop in challenge one for sum
    2. Created for loop and If statement for challenge two.

--- src/assignment.js
+++ src/assignment.js
@@ -13,11 +13,18 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.sumOfNumbers = sumOfNumbers;
 
+function sumOfNumbers(arrayOfNumbers) {
+  
+//return arrayOfNumbers.length;
+   let sum = 0;
+
+for (let i of arrayOfNumbers)
+
+sum += i;
+return sum;
+}
+assignment.sumOfNumbers = sumOfNumbers;
 /**
  * Challenge - 2
  * 
@@ -28,9 +35,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+   // return arrayOfNumbers.length;
+  let count = 0;
+for(let i = 0 ; i < arrayOfNumbers.length; i++) {
+if (arrayOfNumbers[i] % 2 === 0){
+			count++;
+		       }
+					}
+return count;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
